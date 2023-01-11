086d105175fca7da66106b28bd0bddf706ba5c2f
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-10 21:10:35 -0300

    feat: validations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,3 +1,6 @@
 class Department < ApplicationRecord
   has_many :employees
+
+  validates :name, presence: true
+  validates :name, length: { in: 3..50 }  
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,8 @@
 class Employee < ApplicationRecord
   belongs_to :department
   belongs_to :user
+
+  validates :full_name, :email, presence: true
+  validates_format_of :email, with: /\A(\S+)@(.+)\.(\S+)\z/
+  validates :full_name, length: { in: 3..80 }
 end
