6161a627ce89718d54779c450fcc7ee02f08d78d
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-14 00:19:52 -0300

    feat: add mystery_lunch_employee_schedules

---
+++ db/migrate/20230113010728_create_mystery_lunch_employee_schedules.rb
@@ -0,0 +1,10 @@
+class CreateMysteryLunchEmployeeSchedules < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mystery_lunch_employee_schedules do |t|
+      t.integer :selected
+      t.references :employee, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/mystery_lunch_employee_schedules.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :mystery_lunch_employee_schedule do
+    selected { 1 }
+    employee { nil }
+  end
+end
