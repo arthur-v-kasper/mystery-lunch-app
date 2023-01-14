41f6faeb5f07939e84837c329b42d6ea745a6dba
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-14 00:14:07 -0300

    feat: callback create MysteryLunchEmployeeSchedule

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
 class Employee < ApplicationRecord
+  after_create :set_mystery_lunch_employee_schedule
+
   belongs_to :department
   belongs_to :user
 
   validates :full_name, :email, presence: true
   validates_format_of :email, with: /\A(\S+)@(.+)\.(\S+)\z/
   validates :full_name, length: { in: 3..80 }
+
+  private
+
+  def set_mystery_lunch_employee_schedule
+    MysteryLunchEmployeeSchedule.create!(employee_id: id)
+  end
 end
