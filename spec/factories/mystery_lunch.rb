63a3a67fd4f37d1181785091d6d6a90b79d429a8
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-18 22:10:58 -0300

    refactor: add trait to mystery lunch factory

--- spec/factories/mystery_lunch.rb
+++ spec/factories/mystery_lunch.rb
@@ -3,9 +3,27 @@
 FactoryBot.define do
   factory :mystery_lunch do
     year_month { 202_212 }
+    
+    transient do
+      ml_employee_schedule { }
+    end
 
     trait :empty do
       year_month { nil }
+    end
+
+    trait :with_same_employees do
+      after(:create) do |mystery_lunch, evaluator|
+        create(:mystery_lunch_employee, mystery_lunch: mystery_lunch, employee: evaluator.ml_employee_schedule.first.employee)
+        create(:mystery_lunch_employee, mystery_lunch: mystery_lunch, employee: evaluator.ml_employee_schedule.second.employee)
+      end
+    end
+
+    trait :with_others_employees do
+      after(:create) do |mystery_lunch, evaluator|
+        create(:mystery_lunch_employee, mystery_lunch: mystery_lunch, employee: evaluator.ml_employee_schedule.third.employee)
+        create(:mystery_lunch_employee, mystery_lunch: mystery_lunch, employee: evaluator.ml_employee_schedule.fourth.employee)
+      end
     end
   end
 end
