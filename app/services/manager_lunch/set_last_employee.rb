d5f0b9732d7282f90ebf990fbe70e07c823cde05
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-20 00:55:21 -0300

    refactor: add method

--- app/services/manager_lunch/set_last_employee.rb
+++ app/services/manager_lunch/set_last_employee.rb
@@ -4,14 +4,14 @@ module ManagerLunch
   class SetLastEmployee < ApplicationService
     attr_accessor :year_month
 
-    def initialize(year_month)
+    def initialize(year_month = MysteryLunch::CURRENT_YEARMONTH)
       @year_month = year_month
       @employee = last_employee_not_selected
     end
 
     def call
-      department_id = @employee.employee.department.id
-      mystery_lunch = lunch_from_diferent_departament(department_id)
+      department_id = employee_department_id
+      mystery_lunch = lunch_from_diferent_departament(department_id)      
       ManagerLunch::SetEmployee.call(mystery_lunch.mystery_lunch_id,
                                      @employee.employee.id)
     end
@@ -24,6 +24,10 @@ module ManagerLunch
 
     def lunch_from_diferent_departament(department_id)
       ManagerLunch::FindMLunchDiferentDepartament.call(department_id, @year_month)
+    end
+
+    def employee_department_id
+      @employee.employee.department.id
     end
   end
 end
