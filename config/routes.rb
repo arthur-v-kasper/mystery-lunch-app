145e34a1e9f41d1ae233013440c13274c52f4b9c
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-20 00:53:04 -0300

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  get '/departments', to: 'departments#index'
+
+  resources :employees
 end
