72acd298cbf8913c7cb2d74f7696afa4f508359d
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-22 16:04:54 -0300

    chore: add trait to factory

--- spec/factories/mystery_lunch.rb
+++ spec/factories/mystery_lunch.rb
@@ -29,5 +29,24 @@ FactoryBot.define do
                                         employee: evaluator.ml_employee_schedule.fourth.employee)
       end
     end
+
+    trait :pair_employees do
+      after(:create) do |mystery_lunch, _evaluator|
+        mystery_lunch.update!(year_month: MysteryLunch::CURRENT_YEARMONTH)
+        employees = create_list(:employee, 2)
+        create(:mystery_lunch_employee, mystery_lunch:, employee: employees.first)
+        create(:mystery_lunch_employee, mystery_lunch:, employee: employees.second)
+      end
+    end
+
+    trait :trio_employees do
+      after(:create) do |mystery_lunch, _evaluator|
+        mystery_lunch.update!(year_month: MysteryLunch::CURRENT_YEARMONTH)
+        employees = create_list(:employee, 3)
+        create(:mystery_lunch_employee, mystery_lunch:, employee: employees.first)
+        create(:mystery_lunch_employee, mystery_lunch:, employee: employees.second)
+        create(:mystery_lunch_employee, mystery_lunch:, employee: employees.third)
+      end
+    end
   end
 end
