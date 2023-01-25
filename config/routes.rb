980615976f211bbd8fad8999186ad1fd63d717e6
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-24 21:29:17 -0300

    feat: add controller and route to render the frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
+  root to: 'main#index'
   post '/auth', to: 'users#auth'
-
   get '/departments', to: 'departments#index'
-
-  get '/mystery_lunches', to: 'mystery_lunches#index'
-
+  get '/mystery_lunches', to: 'mystery_lunches#index' 
   resources :employees
 end
