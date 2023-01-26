44be5b3eb9cb2d9112a00231925249a0f93e8899
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2023-01-25 21:51:37 -0300

    feat: add search by year month

--- app/controllers/mystery_lunches_controller.rb
+++ app/controllers/mystery_lunches_controller.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
 class MysteryLunchesController < ApplicationController
-  def index
-    @mystery_lunches = MysteryLunch.where(year_month: 202301)    
+  def index    
+    @mystery_lunches = MysteryLunch.where(year_month:)    
+  end
+
+  private
+
+  def year_month
+    year_month = params[:year_month]
+    year_month || MysteryLunch::CURRENT_YEARMONTH
   end
 end
